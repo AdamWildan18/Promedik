f9475e8b29bcfdfbf62775b3571c75428725f44c
Danuwidjaya <sifakomaradanuwidjaya@gmail.com>
2024-03-26 09:18:32 +0700

    Perbaharui tampilan login

--- resources/views/pages/outlet/createExel.blade.php
+++ resources/views/pages/outlet/createExel.blade.php
@@ -155,7 +155,7 @@
     </div>
 
     <footer>
-        Copyright © 2022 &nbsp <a href="https://www.youtube.com/c/mulaidarinull" target="_blank" class="ml-1"> Mulai Dari Null </a> <span> . All rights Reserved</span>
+        Copyright © 2024 &nbsp PT.PROMOSINDO MEDIKA </a> <span> . All rights Reserved</span>
     </footer>
     <div class="overlay action-toggle">
     </div>
